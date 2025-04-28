c39a742fc16dffdc1c8334f08d54ee583a17296d
Skrimper <thedynamitedj@gmail.com>
2025-04-27 23:13:33 -0600

    Built out html and added assets

---
+++ script.js
@@ -0,0 +1,18 @@
+const slideshows = document.querySelectorAll(".slideshow-container");
+
+slideshows.forEach((slideshow, index) => {
+  const slides = slideshow.querySelectorAll(".slide");
+  let current = 0;
+
+  function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+  }
+
+  showSlide(current);
+
+  setInterval(() => {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }, 3000); // change every 3 seconds
+});
